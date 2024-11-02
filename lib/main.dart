e00e509c582af46e4e42875bc3ac2a74d20aab8e
Dnnst89 <dannysoto.cr@gmail.com>
2024-11-02 16:52:22 -0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import "package:first_flutter_app/home_page.dart";
+import "package:flutter/material.dart";
+
+void main() => runApp(const Myapp());
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Flutter Demo",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
